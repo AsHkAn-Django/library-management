d94753d8de6e1b81344d83261785e3ef2ec27259
AsHkaN <ashkan_ahrari@yahoo.com>
2025-07-10 18:45:13 +0300

    fixed the database problem

--- library_management/settings.py
+++ library_management/settings.py
@@ -80,23 +80,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'library_management.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            config("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=True)
-    }
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
